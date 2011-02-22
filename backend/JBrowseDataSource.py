b0988ad39070bda85737b897234c0011c8fbed90
Seren <uemit.seren@gmi-lws06.(none)>
2011-02-22 13:13:08 +0100

    GeneNameeTree class to retrieve gene list added

--- backend/JBrowseDataSource.py
+++ backend/JBrowseDataSource.py
@@ -105,6 +105,7 @@ class DataSource(object):
         self.__jbrowse_tracks_folder = jbrowse_tracks_folder
         self.__track_key = track_key
         self.__chromosomeSources = {}
+        self.__genenametree = GeneNameTree(jbrowse_tracks_folder)
         dirList=os.listdir(self.__jbrowse_tracks_folder+"/data/tracks/")
         for fname in dirList:
             self.__initChromocomeSources(fname)
@@ -117,14 +118,125 @@ class DataSource(object):
     
     
     def __initChromocomeSources(self,fname):
-        fp = open(self._getChromosomeTrackFolder(fname)+'trackData.json')
+        fp = open(self.__getChromosomeTrackFolder(fname)+'trackData.json')
         trackData = simplejson.load(fp)
         fp.close()
         self.__chromosomeSources[fname] = ChromosomeData(self.__jbrowse_tracks_folder,trackData)
     
-    def _getChromosomeTrackFolder(self,chromosome):
+    def __getChromosomeTrackFolder(self,chromosome):
         return self.__jbrowse_tracks_folder + '/data/tracks/%s/%s/' % (chromosome,self.__track_key)
     
-
     
+    def getGeneFromName(self,query):
+        return self.__genenametree.getGeneFromName(query)
+    
+    def getGenesFromQuery(self,query):
+        return self.__genenametree.getGenesFromQuery(query)
+
+class GeneNameTree(object):
+    
+    def __init__(self,jbrowse_tracks_folder):
+        self.__jbrowse_tracks_folder = jbrowse_tracks_folder
+        self.__nodes = self.__loadRootNode()
+    
+    def __loadRootNode(self):
+        fp = open(self.__getRootNodeFolder()+'root.json')
+        nodes = simplejson.load(fp)
+        fp.close()
+        return nodes
+         
+    def getGeneFromName(self,query):
+        query = query.lower()
+        qStart = 0
+        path = []
+        node = self.__nodes
+        while True:
+            childIndex = self.__binary_search(node,query[qStart])
+            if childIndex < 0: return 
+            path.append(childIndex)
+            if isinstance(node[childIndex][0],int):
+                node[childIndex] = self.__loadNode(path)
+            node = node[childIndex]
+        
+            if query[qStart:qStart+len(node[0])] != node[0][0:min([len(node[0]),len(query) - qStart])]:
+                return 
+            qStart = qStart + len(node[0])
+            if (qStart >=len(query)):
+                if isinstance(node[1],list):
+                    for gene in node[1]:
+                        if  gene[1].lower() == query.lower():
+                            gene[5] = ''
+                            return gene
+                return
+    
+    def getGenesFromQuery(self,query):
+        genes = []
+        query = query.lower()
+        qStart = 0
+        path = []
+        node = self.__nodes
+        while True:
+            childIndex = self.__binary_search(node,query[qStart])
+            if childIndex < 0: return
+            path.append(childIndex)
+            if isinstance(node[childIndex][0],int):
+                node[childIndex] = self.__loadNode(path)
+            node = node[childIndex]
+            qStart = qStart + len(node[0])
+            if qStart >= len(query):
+                genes = self.__getAllGenesFromNode(node,genes,path)
+                return genes
+    
+    def __getAllGenesFromNode(self,node,genes,path):
+        if isinstance(node[1],list):
+            for gene in node[1]:
+                gene[5] = ''
+                genes.append(gene)
+        elif isinstance(node[0],int):
+            node = self.__loadNode(path)
+        
+        for i,sub_nodes in enumerate(node[2:]):
+            path.append(i+2)
+            genes = self.__getAllGenesFromNode(sub_nodes, genes,path) 
+        return genes
+        
+    def __getRootNodeFolder(self):
+        return self.__jbrowse_tracks_folder + '/data/names/'
+    
+    def __getPathToPrefix(self,path):
+        node = self.__nodes
+        result = ''
+        for i,index in enumerate(path):
+            if isinstance(node[index][0],int):
+                result = result + str(node[index][1])
+            else:
+                result = result + str(node[index][0])
+            node = node[index]
+        return result
+    
+    def __loadNode(self,path):
+        fp = open(self.__getRootNodeFolder() + 'lazy-%s.json' % str(self.__getPathToPrefix(path)))
+        node = simplejson.load(fp)
+        fp.close()
+        return node
+        
+    def __binary_search(self,list,char):
+        low = 2
+        high = len(list) - 1
+        while (low <= high):
+            mid = (low + high) >> 1
+            if isinstance(list[mid][0],int):
+                midVal = list[mid][1][0]
+            else:
+                midVal = list[mid][0][0]
+            if midVal < char:
+                low = mid + 1
+            elif midVal > char:
+                high = mid - 1
+            else:
+                return mid
+        
+        return -(low +1)
+            
+            
     
